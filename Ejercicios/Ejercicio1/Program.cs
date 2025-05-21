9244d7c9053b54ba1c4a306e50e2cdb9688228e4
Vitale71 <lucasvitale71@gmail.com>
2025-05-21 19:23:46 -0300

    .

---
+++ Ejercicios/Ejercicio1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int encuestadores, encuestados = 0, contB = 0, contM = 0, contA = 0, contT = 0;
+            Console.WriteLine("ingrese la cantidad de encuestadores que va haber");
+            encuestadores = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < encuestadores; i++)
+            {
+                int contP = 0, bicicleta = 0, motocicleta = 0, automovil = 0, transporte = 0, acumB = 0, acumM = 0;
+                Console.WriteLine("ingrese la primera opinion si hay una persona");
+                Console.WriteLine("si su uso mas frecuente es bicicleta (0), motocicleta (1), automovil (2), transporte publico (3)"
+                    + "o ponga -1 para salir");
+                int persona = Convert.ToInt32(Console.ReadLine());
+
+                #region iterar encuestados
+                while (persona != -1)
+                {
+                    contP++;
+                    switch (persona)
+                    {
+                        case 0: 
+                            Console.WriteLine("ingrese distancia aproximada que utiliza la bicicleta");
+                            bicicleta++;
+                            int distanciaB = Convert.ToInt32(Console.ReadLine());
+                            acumB = acumB + distanciaB;
+                            break;
+                        
+                        case 1:
+                            Console.WriteLine("ingrese distancia aproximada que utiliza la motocicleta");
+                            motocicleta++;
+                            int distanciaM = Convert.ToInt32(Console.ReadLine());
+                            acumM += distanciaM;
+                            break;
+
+                        case 2:
+                            Console.WriteLine("ingrese distancia aproximada que utiliza el automovil");
+                            automovil++;
+                            int distanciaA = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        case 3:
+                            Console.WriteLine("ingrese distancia aproximada que utiliza el transporte");
+                            transporte++;
+                            int distanciaT = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        default:
+                            Console.WriteLine("numero no valido");
+                            contP--;
+                        break;
+                    }
+                    Console.WriteLine("ingrese la siguiente opinion si hay una persona o ponga -1 si no hay persona");
+                    Console.WriteLine("si su uso mas frecuente es bicicleta (0), motocicleta (1), automovil (2), transporte publico (4)");
+                    persona = Convert.ToInt32(Console.ReadLine());
+                }
+                #endregion
+
+                #region entrega de datos a la empresa
+                Console.WriteLine("el encuestador {0} entrega los siguientes resultados" , i);
+                Console.WriteLine("cantidad de encuestados: " + contP);
+                Console.WriteLine("cantidad de personas que usan bicicleta: " + bicicleta);
+                Console.WriteLine("cantidad de personas que usan motocicleta: " + motocicleta);
+                Console.WriteLine("cantidad de personas que usan automovil: " + automovil);
+                Console.WriteLine("cantidad de personas que usan transporte publico: " + transporte);
+                if (bicicleta > 0)
+                {
+                    Console.WriteLine("distancia promedio recorrida poara el caso de los usuarios con bicicleta {0:f1}", (acumB / bicicleta));
+                }
+                else
+                {
+                    Console.WriteLine("no hay distancia promedio recorrida para el caso de bicicletas");
+                }
+                if (motocicleta > 0)
+                {
+                    Console.WriteLine("distancia promedio recorrida poara el caso de los usuarios con motocicleta {0:f1}", (acumM / motocicleta));
+                }
+                else
+                {
+                    Console.WriteLine("no hay distancia promedio recorrida para el caso de motocicletas");
+                }
+                #endregion
+
+                #region actualizar contadores
+                encuestados += contP;
+                contB += bicicleta;
+                contM += motocicleta;
+                contA += automovil;
+                contT += transporte;
+                #endregion
+
+            }
+            Console.WriteLine("la cantidad de encuestados total es de: " + encuestados);
+            Console.WriteLine("El porcentaje de personas total que utilizan bicicleta: {0:f1} " ,((contB * 100) / encuestados));
+            Console.WriteLine("El porcentaje de personas total que utilizan motocicleta: {0:f1} ", ((contM * 100) / encuestados));
+            Console.WriteLine("El porcentaje de personas total que utilizan automovil: {0:f1} ", ((contA * 100) / encuestados));
+            Console.WriteLine("El porcentaje de personas total que utilizan transporte publico: {0:f1} ", ((contT * 100) / encuestados));
+        }
+    }
+}
---
+++ Ejercicios/Ejercicio2/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float recaudacion = 0;
+            int motoT = 0, autoT = 0, utilitarioT = 0, camionT = 0;
+            Console.WriteLine("ingrese un numero cualquiera si hay una zona de estacionamiento o -1 para salir");
+            int zonas = Convert.ToInt32(Console.ReadLine());
+            while (zonas != -1)
+            {
+                #region ingreso de datos
+                float acumCoste = 0, porcentajeMoto = 0, porcentajeAuto = 0, porcentajeUtilitario = 0, porcentajeCamion = 0, motoCosto = 0, autoCosto =0, utilitarioCosto = 0, camionCosto = 0;
+                Console.WriteLine("ingrese el valor por estacionamiento de motos");
+                int motosP = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ingrese el valor por estacionamiento de autos");
+                int autosP = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ingrese el valor por estacionamiento de utilitarios");
+                int utilitariosP = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ingrese el valor por estacionamiento de camiones");
+                int camionesP = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ingrese el costo por hora");
+                int coste = Convert.ToInt32(Console.ReadLine());
+                #endregion
+
+                #region iterar zona
+                Console.WriteLine("ingrese el vehiculo que haya entrado al estacionamiento o -1 si se finalizo el dia");
+                Console.WriteLine("si es moto (0), si es auto (1), si es utilitario (2), si es camion (3)");
+                int zona = Convert.ToInt32(Console.ReadLine());
+                while (zona != -1)
+                {
+                    float calculoCoste = 0;
+                    switch (zona)
+                    {
+                        case 0:
+                            motoT++;
+                            Console.WriteLine("ingrese las horas que estuvo adquirido");
+                            int horas = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("ingrese los minutos");
+                            int minutos = Convert.ToInt32(Console.ReadLine());
+                            calculoCoste = horas*coste;
+                            if (minutos > 0)
+                            {
+                                calculoCoste += (coste / 60) * minutos;
+                            }
+                            calculoCoste += motosP;
+                            Console.WriteLine("el coste por hora es de: ${0}, el vehiculo estuvo este tiempo {1}:{2}" ,coste ,horas, minutos);
+                            Console.WriteLine("el valor total por las horas ocupadas es de: $" +calculoCoste);
+                            acumCoste += calculoCoste;
+                            motoCosto += calculoCoste;
+                            recaudacion += calculoCoste;
+                            break;
+
+                        case 1:
+                            autoT++;
+                            Console.WriteLine("ingrese las horas que estuvo adquirido");
+                            horas = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("ingrese los minutos");
+                            minutos = Convert.ToInt32(Console.ReadLine());
+                            calculoCoste = horas * coste;
+                            if (minutos > 0)
+                            {
+                                calculoCoste += (coste / 60) * minutos;
+                            }
+                            calculoCoste += autosP;
+                            Console.WriteLine("el coste por hora es de: ${0}, el vehiculo estuvo este tiempo {1}:{2}", coste, horas, minutos);
+                            Console.WriteLine("el valor total por las horas ocupadas es de: $" + calculoCoste);
+                            acumCoste += calculoCoste;
+                            autoCosto += calculoCoste;
+                            recaudacion += calculoCoste;
+                            break;
+
+                        case 2:
+                            utilitarioT++;
+                            Console.WriteLine("ingrese las horas que estuvo adquirido");
+                            horas = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("ingrese los minutos");
+                            minutos = Convert.ToInt32(Console.ReadLine());
+                            calculoCoste = horas * coste;
+                            if (minutos > 0)
+                            {
+                                calculoCoste += (coste / 60) * minutos;
+                            }
+                            calculoCoste += utilitariosP;
+                            Console.WriteLine("el coste por hora es de: ${0}, el vehiculo estuvo este tiempo {1}:{2}", coste, horas, minutos);
+                            Console.WriteLine("el valor total por las horas ocupadas es de: $" + calculoCoste);
+                            acumCoste += calculoCoste;
+                            utilitarioCosto += calculoCoste;
+                            recaudacion += calculoCoste;
+                            break;
+
+                        case 3:
+                            camionT++;
+                            Console.WriteLine("ingrese las horas que estuvo adquirido");
+                            horas = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("ingrese los minutos");
+                            minutos = Convert.ToInt32(Console.ReadLine());
+                            calculoCoste = horas * coste;
+                            if (minutos > 0)
+                            {
+                                calculoCoste += (coste / 60) * minutos;
+                            }
+                            calculoCoste += camionesP;
+                            Console.WriteLine("el coste por hora es de: ${0}, el vehiculo estuvo este tiempo {1}:{2}", coste, horas, minutos);
+                            Console.WriteLine("el valor total por las horas ocupadas es de: $" + calculoCoste);
+                            acumCoste += calculoCoste;
+                            camionCosto += calculoCoste;
+                            recaudacion += calculoCoste;
+                            break;
+
+                        default:
+                            Console.WriteLine("numero no valido vuelva a intentarlo");
+                            break;
+                    }
+                    Console.WriteLine("ingrese el vehiculo que haya entrado al estacionamiento o -1 si se finalizo el dia");
+                    Console.WriteLine("si es moto (0), si es auto (1), si es utilitario (2), si es camion (3)");
+                    zona = Convert.ToInt32(Console.ReadLine());
+                }
+                #endregion
+
+                #region mostrar jornada del dia
+                Console.WriteLine("la recaudacion total de esta zona fue de: $" +acumCoste);
+                if (motoCosto > 0)
+                {
+                    porcentajeMoto = (motoCosto * 100) / acumCoste;
+                    Console.WriteLine("El porcentaje de recaudacion de motos es de: {0:f1}%", porcentajeMoto);
+                }
+                else
+                {
+                    Console.WriteLine("El porcentaje de recaudacion de motos es de : 0.0%");
+                }
+                if (autoCosto > 0)
+                {
+                    porcentajeAuto = (autoCosto * 100) / acumCoste;
+                    Console.WriteLine("El porcentaje de recaudacion de autos es de: {0:f1}%", porcentajeAuto);
+                }
+                else
+                {
+                    Console.WriteLine("El porcentaje de recaudacion de autos es de : 0.0%");
+                }
+                if (utilitarioCosto > 0)
+                {
+                    porcentajeUtilitario = (utilitarioCosto * 100) / acumCoste;
+                    Console.WriteLine("El porcentaje de recaudacion de utilitario es de: {0:f1}%", porcentajeUtilitario);
+                }
+                else
+                {
+                    Console.WriteLine("El porcentaje de recaudacion de utilitario es de : 0.0%");
+                }
+                if (camionCosto > 0)
+                {
+                    porcentajeCamion = (camionCosto * 100) / acumCoste;
+                    Console.WriteLine("El porcentaje de recaudacion de camiones es de: {0:f1}%", porcentajeCamion);
+                }
+                else
+                {
+                    Console.WriteLine("El porcentaje de recaudacion de camiones es de : 0.0%");
+                }
+                #endregion
+
+                Console.WriteLine("ingrese un numero cualquiera si hay una zona de estacionamiento o -1 para salir");
+                zonas = Convert.ToInt32(Console.ReadLine());
+            }
+
+            #region informe de la empresa
+            Console.WriteLine("la recaudacion total de todas las zonas fue de: $" + recaudacion);
+            int totalVehiculos = motoT + autoT + utilitarioT + camionT;
+            if (motoT > 0)
+            {
+                float porcentajeMotoT = (motoT * 100f) / totalVehiculos;
+                Console.WriteLine("El porcentaje de motos total es de: {0:f1}", porcentajeMotoT);
+            }
+            else
+            {
+                Console.WriteLine("El porcentaje de motos total es de: 0");
+            }
+            if (autoT > 0)
+            {
+                float porcentajeAutoT = (autoT * 100f) / totalVehiculos;
+                Console.WriteLine("El porcentaje de autos total es de: {0:f1}", porcentajeAutoT);
+            }
+            else
+            {
+                Console.WriteLine("El porcentaje de autos total es de: 0");
+            }
+            if (utilitarioT > 0)
+            {
+                float porcentajeUtilitarioT = (utilitarioT * 100f) / totalVehiculos;
+                Console.WriteLine("El porcentaje de utilitarios total es de: {0:f1}", porcentajeUtilitarioT);
+            }
+            else
+            {
+                Console.WriteLine("El porcentaje de utilitario total es de: 0");
+            }
+            if (camionT > 0)
+            {
+                float porcentajeCamionT = (camionT * 100f) / totalVehiculos;
+                Console.WriteLine("El porcentaje de camiones total es de: {0:f1}", porcentajeCamionT);
+            }
+            else
+            {
+                Console.WriteLine("El porcentaje de camiones total es de : 0");
+            }
+            #endregion
+        }
+    }
+}
